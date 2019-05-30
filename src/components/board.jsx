c7ab5c2c0d58a78943cb5091fea8444d6b834cc9
Sam Backus <Samuel.Backus@gmail.com>
2019-05-30 17:42:09 -0400

    organizing components into separate files and adding controls for picking ai players

---
+++ src/components/board.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Square from './square'
+// ========================================
+
+class Board extends React.Component {
+  renderSquare(square_num) {
+    return (
+      <Square
+        value={this.props.squares[square_num]}
+        onClick={()=> {this.props.onClick(this.props.board_num,square_num)}}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board
